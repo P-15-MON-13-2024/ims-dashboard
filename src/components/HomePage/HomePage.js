df4704215a7891973cbbd615e1d2f2c40c6eac88
keymii <abhi.vansh2017@gmail.com>
2024-03-27 15:25:51 +0530

    add issued items api to HomePage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 import LongListItem from '../LongListItem/LongListItem';
 import './HomePage.css';
 import '../SmallBlock/SmallBlock';
 import SmallBlock from '../SmallBlock/SmallBlock';
 import RecentActivityBlock from '../RecentActivityBlock/RecentActivityBlock';
-const HomePage = () => (
+import { format } from 'date-fns';
+
+function HomePage(){
+  const [issuedData, setIssuedData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/api/dashboard/issued-items/');
+        const jsonData = await response.json();
+        setIssuedData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return(
   <div className="HomePage">
     <div className='LeftPage'>
       <div className='recentSearchArea'>
@@ -16,46 +34,25 @@ const HomePage = () => (
       </div>
       <div className='IssuedItemsArea'>
         <h2>Issued Items</h2>
-        <LongListItem imageUrl={""}>
-          <h3 style={{'margin-top':'0'}}>Header</h3>
-          SomeText<br/>
-          somemoretext
-        </LongListItem>        <LongListItem imageUrl={""}>
-          <h3 style={{'margin-top':'0'}}>Header</h3>
-          SomeText<br/>
-          somemoretext
-        </LongListItem>        <LongListItem imageUrl={""}>
-          <h3 style={{'margin-top':'0'}}>Header</h3>
-          SomeText<br/>
-          somemoretext
-        </LongListItem>        <LongListItem imageUrl={""}>
-          <h3 style={{'margin-top':'0'}}>Header</h3>
-          SomeText<br/>
-          somemoretext
-        </LongListItem>        <LongListItem imageUrl={""}>
-          <h3 style={{'margin-top':'0'}}>Header</h3>
-          SomeText<br/>
-          somemoretext
-        </LongListItem>        <LongListItem imageUrl={""}>
-          <h3 style={{'margin-top':'0'}}>Header</h3>
-          SomeText<br/>
-          somemoretext
-        </LongListItem>        <LongListItem imageUrl={""}>
-          <h3 style={{'margin-top':'0'}}>Header</h3>
-          SomeText<br/>
-          somemoretext
-        </LongListItem>        <LongListItem imageUrl={""}>
-          <h3 style={{'margin-top':'0'}}>Header</h3>
-          SomeText<br/>
-          somemoretext
-        </LongListItem>
+        {
+          issuedData.map((item, index)=>(
+            <LongListItem imageUrl={""}>
+              <h3 style={{'marginTop':'0', 'marginBottom':'0'}}>{item['item_name']}</h3>
+              {item['item_serial_id']}<br/>
+              <p>
+                Issued By: {item['user_name']}<br/>
+                Issued On: {format(new Date(item['issue_time']),'MMMM dd, yyyy hh:mm a')}
+              </p>
+            </LongListItem> 
+          ))
+        }
       </div>
     </div>
     {/* <div className='RightPage'>
       <RecentActivityBlock/>
     </div> */}
   </div>
-);
+)};
 
 HomePage.propTypes = {};
 
